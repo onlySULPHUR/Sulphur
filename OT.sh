3c1798684ff03efa5a4f123285ac0cdc708e8b4b
onlySULPHUR <60334841+onlySULPHUR@users.noreply.github.com>
2021-01-03 14:36:28 +0530

    Create OT.sh

---
+++ OT.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#t.me/onlysulphur
+echo -e "\e[93mDont Use This Program If Your Device Is Android 7 or Below"
+sleep 4
+ pkg install python -y && pkg install tsu -y && termux-setup-storage && python -m pip install requests && pip install colorama 
+sleep 1
+sleep 1
+echo -e "\e[93m If The Process Took More Than 2 Minites than it is succeeded otherwise uninstall termux and do this process again"
+sleep 1
+exit 0

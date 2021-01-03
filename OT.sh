268b1b64bfbb451b33f05da4ea8601f587a09d73
onlySULPHUR <60334841+onlySULPHUR@users.noreply.github.com>
2021-01-03 14:58:09 +0530

    Update OT.sh

--- OT.sh
+++ OT.sh
@@ -8,3 +8,18 @@ sleep 1
 echo -e "\e[93m If The Process Took More Than 2 Minites than it is succeeded otherwise uninstall termux and do this process again"
 sleep 1
 exit 0
+#!/usr/bin/env bash
+#t.me/onlysulphur
+mkdir /data/data/com.termux/files/home/Sulphur/pytransform
+mv /data/data/com.termux/files/home/Sulphur/_pytransform.so /data/data/com.termux/files/home/Sulphur/pytransform/
+mv /data/data/com.termux/files/home/Sulphur/__init__.py /data/data/com.termux/files/home/Sulphur/pytransform/
+mv /data/data/com.termux/files/home/Sulphur/__init__.py /data/data/com.termux/files/home/Sulphur/pytransform/
+
+echo -e "\e[93mDont Use This Program If Your Device Is Android 7 or Below"
+sleep 4
+ pkg install python -y && pkg install tsu -y && termux-setup-storage && python -m pip install requests && pip install colorama 
+sleep 1
+sleep 1
+echo -e "\e[93m If The Process Took More Than 2 Minites than it is succeeded otherwise uninstall termux and do this process again"
+sleep 1
+exit 0

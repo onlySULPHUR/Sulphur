ff8e8017d144bac828608542178f8710dcc8f25b
Sulphur <60334841+onlySULPHUR@users.noreply.github.com>
2021-02-01 20:05:27 +0530

    Update OT.sh

--- OT.sh
+++ OT.sh
@@ -1,19 +1,11 @@
-
-
-
- 
-
-
+#Dont Check This File Because It Is not encrypted 🤣🤣🤣🤣🤣
 #!/usr/bin/env bash
 #t.me/onlysulphur
 mkdir /data/data/com.termux/files/home/Sulphur/pytransform
 mv /data/data/com.termux/files/home/Sulphur/_pytransform.so /data/data/com.termux/files/home/Sulphur/pytransform/
 mv /data/data/com.termux/files/home/Sulphur/__init__.py /data/data/com.termux/files/home/Sulphur/pytransform/
 
-
-echo -e "\e[93mDont Use This Program If Your Device Is Android 7 or Below"
-sleep 4
- pkg install python -y && pkg install tsu -y && termux-setup-storage && python -m pip install requests && pip install colorama 
+pkg install python -y && pkg install tsu -y && termux-setup-storage && python -m pip install requests && pip install colorama 
 sleep 1
 sleep 1
 echo -e "\e[93m If The Process Took More Than 2 Minites than it is succeeded otherwise uninstall termux and do this process again"

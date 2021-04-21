0c32281675b05e40e46b28ff19135ccb2e1ecc8e
Sulphur <60334841+onlySULPHUR@users.noreply.github.com>
2021-04-21 11:25:56 +0530

    Update OT.sh

--- OT.sh
+++ OT.sh
@@ -1,6 +1,2 @@
-#Dont Check This File Because It Is not encrypted 🤣🤣🤣🤣🤣
-#!/usr/bin/env bash
-#t.me/onlysulphur
-mkdir /data/data/com.termux/files/home/Sulphur/pytransform
-mv /data/data/com.termux/files/home/Sulphur/_pytransform.so /data/data/com.termux/files/home/Sulphur/pytransform/
-mv /data/data/com.termux/files/home/Sulphur/__init__.py /data/data/com.termux/files/home/Sulphur/pytransform/ 
+
+su -c am start -n org.telegram.messenger/org.telegram.ui.LaunchActivity https://t.me/onlysulphur
